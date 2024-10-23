325fdba578333ac1e6511f9f2f8cd71cb58db1ed
Ashish <ashishjadhav3300@gmail.com>
2024-10-23 15:09:19 +0530

    eslint bug fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        ignoreDuringBuilds: false
+        ignoreDuringBuilds: false,  
+    },
+    typescript: {
+        ignoreBuildErrors: false
     }
 };
 
