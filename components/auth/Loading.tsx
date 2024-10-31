ad2d1f95d7cac6c4002caaaf7db3aad3d3f9efa0
Ashish <ashishjadhav3300@gmail.com>
2024-10-31 17:50:24 +0530

    lay

--- components/auth/Loading.tsx
+++ components/auth/Loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='flex items-center justify-center h-screen w-full'>
+    <div className='flex items-center justify-center h-screen w-full max-md:px-4'>
         <h1 className='text-[72px] font-semibold text-primary-500 animate-pulse duration-700'>ITSA Innovision</h1>
     </div>
   )
