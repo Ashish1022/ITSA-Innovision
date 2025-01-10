29eb5abea28dae261964bcad9e8ec19c4babccf9
Ashish Jadhav <111486032+Ashish1022@users.noreply.github.com>
2025-01-09 23:11:45 -0800

    theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
     <html lang="en" className="overflow-auto scrollbar-hide">
       <ConvexClerkProvider>
         <body className={poppins.variable}>
-          <ThemeProvider attribute='class' defaultTheme="system" enableSystem>
+          <ThemeProvider attribute='class' defaultTheme="dark" enableSystem>
           {children}
           <Toaster/>
           <Analytics/>
