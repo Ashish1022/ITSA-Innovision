4ecb52338968f549d6fbc9a77f648e55f262eb98
Ashish Jadhav <111486032+Ashish1022@users.noreply.github.com>
2025-01-12 22:37:09 -0800

    home

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -30,7 +30,7 @@ const Home = () => {
                   Register
                 </Link>
               </Button>
-              <Button asChild className="rounded-full  max-md:w-full bg-grey-500/10 border border-primary-500" size="lg">
+              <Button asChild className="rounded-full  max-md:w-full bg-grey-500/10 border border-primary-500 text-black-1" size="lg">
                 <Link href='/gallery'>
                   Explore
                 </Link>
